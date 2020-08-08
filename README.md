564f1689b896c5de84c04b2f660fbcd3383663b7
Vitor Esposito <vitor_esposito@hotmail.com>
2020-08-08 14:57:22 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,51 @@
+# Lumen Ala Performance
+
+[![Latest Version](https://img.shields.io/github/v/release/kiwfy/lumen-ala-performance.svg?style=flat-square)](https://github.com/kiwfy/lumen-ala-performance/releases)
+[![PRs Welcome](https://img.shields.io/badge/PRs-welcome-brightgreen.svg?style=flat-square)](http://makeapullrequest.com)
+
+Python code using locust to get domain performance
+
+### Installation
+
+Requires [Python](https://www.python.org/).
+
+The recommended way to install is through [Pip](https://pypi.org/project/pip/).
+
+```sh
+pip install -r requirements.txt
+```
+
+```sh
+cp .env.example .env
+```
+
+Put values in **CONTEXT**, **SECRET** and **TOKEN** environment because the script need to authenticate and verify all CRUD routes (Get this config in Lumen-Ala project).
+
+### Run Cli interface
+
+Run the file domainperformance.py using [Locust](https://locust.io/)
+
+```sh
+locust -f domainperformance.py
+```
+
+And access [Localhost dashboard](http://localhost:8089/) to start the Locust
+
+### Run in background
+
+This example run with 1000 users and spawned 10 per second for 1 minute.
+
+```sh
+locust -f domainperformance.py -H {Your Host and Port Here} -u 1000 -r 10 -t 1m --headless --csv report
+``` 
+
+After running this command, you can consulting 3 files *report_failures.csv*, *report_stats.csv* and *report_stats_history.csv* to validate the performance test.
+
+### Development
+
+Want to contribute? Great!
+
+The project using a simple code.
+Make a change in your file and be careful with your updates!
+
+**Kiwfy - Open your code, open your mind!**
---
+++ domainperformance.py
@@ -0,0 +1,115 @@
+import json
+import os
+import requests
+from dotenv import load_dotenv
+from locust import between, events, HttpUser, task
+
+class DomainPerformance(HttpUser):
+    load_dotenv()
+
+    authToken = None
+    context = os.getenv('CONTEXT')
+    domain = os.getenv('DOMAIN')
+    filterList = os.getenv('FILTER_LIST')
+    host = os.getenv('HOST')
+    insertBody = json.loads(os.getenv('INSERT_DATA'))
+    objectId = None
+    objectIdToDelete = None
+    secret = os.getenv('SECRET')
+    token = os.getenv('TOKEN')
+    updateData = os.getenv('UPDATE_DATA')
+    wait_time = between(5, 15)
+
+    def on_start(self):
+        request = self.client.post(
+            '/auth/generate',
+            {
+                'token': self.token,
+                'secret': self.secret
+            }
+        )
+
+        self.authToken = json.loads(request.content)['data']['token']
+
+    @task
+    def healthCheck(self):
+        self.client.get('/health')
+
+    @task
+    def list(self):
+        self.client.request(
+            'GET',
+            '/' + self.domain + '/list',
+            headers={'Authorization': self.authToken, 'Context': self.context}
+        )
+
+    @task
+    def detail(self):
+        self.client.request(
+            'GET',
+            '/' + self.domain + '/detail/' + self.objectId,
+            headers={'Authorization': self.authToken, 'Context': self.context}
+        )
+
+    @task
+    def edit(self):
+        self.client.request(
+            'PATCH',
+            '/' + self.domain + '/edit/' + self.objectId,
+            headers={'Authorization': self.authToken, 'Context': self.context},
+            data=self.updateData
+        )
+
+    @task
+    def insert(self):
+        self.client.request(
+            'POST',
+            '/' + self.domain + '/add/',
+            headers={'Authorization': self.authToken, 'Context': self.context},
+            data=self.insertBody
+        )
+
+    @task
+    def listWithParam(self):
+        self.client.request(
+            'GET',
+            '/' + self.domain + '/list' + self.filterList,
+            headers={'Authorization': self.authToken, 'Context': self.context}
+        )
+
+    @task
+    def bulk(self):
+        self.client.request(
+            'POST',
+            '/' + self.domain + '/bulk/',
+            headers={'Authorization': self.authToken, 'Context': self.context},
+            data={'ids[]': [self.objectId]}
+        )
+
+    @task
+    def listDelete(self):
+        self.client.request(
+            'GET',
+            '/' + self.domain + '/dead_list/',
+            headers={'Authorization': self.authToken, 'Context': self.context}
+        )
+
+@events.init.add_listener
+def on_locust_init(environment, **_kwargs):
+    request = requests.post(
+        DomainPerformance.host + '/auth/generate',
+        data={
+            'token': DomainPerformance.token,
+            'secret': DomainPerformance.secret
+        }
+    )
+
+    token = json.loads(request.content)['data']['token']
+
+    request = requests.post(
+        DomainPerformance.host + '/' + DomainPerformance.domain + '/add',
+        headers={'Authorization': token, 'Context': DomainPerformance.context},
+        data=DomainPerformance.insertBody
+    )
+
+    DomainPerformance.objectId = json.loads(request.content)['data']['id']
